725528dec857e378880f610302455ae14528ab19
jishan1019 <imranislamjishan200@gmail.com>
2023-02-18 19:37:16 +0600

    blog page complite

---
+++ js/item__calculate.js
@@ -0,0 +1,16 @@
+function inputToNumber(inputNmuberIdOne, inputNmuberIdTwo){
+    const inputElementOne = document.getElementById(inputNmuberIdOne).value;
+    const inputNumberOne = parseInt(inputElementOne);
+
+    const inputElementTwo = document.getElementById(inputNmuberIdOne).value;
+    const inputNumberTwo = parseInt(inputElementTwo);
+
+    const twoInputMultipication = inputNumberOne * inputNumberTwo;
+    return twoInputMultipication;
+    
+}
+
+document.getElementById('btn-triangle').addEventListener('click', function(){
+    
+})
+
