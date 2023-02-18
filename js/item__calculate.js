e2b33f64959414f0e7be4eddb1844b975b4d1685
jishan1019 <imranislamjishan200@gmail.com>
2023-02-18 20:34:48 +0600

    calculate table add done

--- js/item__calculate.js
+++ js/item__calculate.js
@@ -1,21 +1,14 @@
 function inputToNumber(inputNmuberIdOne, inputNmuberIdTwo){
 
     const inputElementOne = document.getElementById(inputNmuberIdOne).value;
+    inputElementOne.value = '';
     const inputNumberOne = parseInt(inputElementOne);
 
     const inputElementTwo = document.getElementById(inputNmuberIdTwo).value;
+    inputElementTwo.value = '';
     const inputNumberTwo = parseInt(inputElementTwo);
-
-    if(inputElementOne.length <= 1 && inputElementTwo.length <=1){
-        alert('Please Input Number');
-        return
-    }else if(inputNumberOne <= 0 && inputNumberTwo<= 0){
-        alert('Please Input Positive Number');
-        return
-    }else{
-        const twoInputMultipication = inputNumberOne * inputNumberTwo;
-        return twoInputMultipication;
-    }
+    const twoInputMultipication = inputNumberOne * inputNumberTwo;
+    return twoInputMultipication;
 }
 
 function getCardItemName(cardNameId){
@@ -44,12 +37,14 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
 
     const triangleInput = inputToNumber('traingle-input-b','traingle-input-h')
 
-    const traingleInputResult = (triangleInput*0.5).toFixed(2);
-    const triangleName = getCardItemName('triangle-titel');
-    appendElementTable(traingleInputResult,triangleName);
-
-
-
+    if(triangleInput <= 0){
+        alert('Please Provide Number')
+        return
+    }else{
+        const traingleInputResult = (triangleInput*0.5).toFixed(2);
+        const triangleName = getCardItemName('triangle-titel');
+        appendElementTable(traingleInputResult,triangleName);
+    }
 
 })
 
