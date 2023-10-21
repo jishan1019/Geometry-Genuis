94097d48c4a31a33d75b9f83df64e065447748ef
jishan1019 <imranislamjishan200@gmail.com>
2023-10-21 11:25:06 +0600

    conver metter problem solve

--- js/hover.js
+++ js/hover.js
@@ -1,27 +1,48 @@
-function random_bg_color(elementIdHover) { 
-    var x = Math.floor(Math.random() * 256); var y = Math.floor(Math.random() * 256); var z = Math.floor(Math.random() * 256); var bgColor = "rgb(" + x + "," + y + "," + z + ")"; console.log(bgColor); document.getElementById(elementIdHover).style.background = bgColor;
+function random_bg_color(elementIdHover) {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  document.getElementById(elementIdHover).style.background = bgColor;
 }
 
-document.getElementById('HoverColor').addEventListener('mouseenter',function(){
-    document.getElementById('HoverColor').style.backgroundColor = random_bg_color('HoverColor');
-})
+document
+  .getElementById("HoverColor")
+  .addEventListener("mouseenter", function () {
+    document.getElementById("HoverColor").style.backgroundColor =
+      random_bg_color("HoverColor");
+  });
 
-document.getElementById('hoverCardTwo').addEventListener('mouseenter',function(){
-    document.getElementById('hoverCardTwo').style.backgroundColor = random_bg_color('hoverCardTwo');
-})
+document
+  .getElementById("hoverCardTwo")
+  .addEventListener("mouseenter", function () {
+    document.getElementById("hoverCardTwo").style.backgroundColor =
+      random_bg_color("hoverCardTwo");
+  });
 
+document
+  .getElementById("hoverCardThree")
+  .addEventListener("mouseenter", function () {
+    document.getElementById("hoverCardThree").style.backgroundColor =
+      random_bg_color("hoverCardThree");
+  });
+document
+  .getElementById("hoverCardFour")
+  .addEventListener("mouseenter", function () {
+    document.getElementById("hoverCardFour").style.backgroundColor =
+      random_bg_color("hoverCardFour");
+  });
 
-document.getElementById('hoverCardThree').addEventListener('mouseenter',function(){
-    document.getElementById('hoverCardThree').style.backgroundColor = random_bg_color('hoverCardThree');
-})
-document.getElementById('hoverCardFour').addEventListener('mouseenter',function(){
-    document.getElementById('hoverCardFour').style.backgroundColor = random_bg_color('hoverCardFour');
-})
+document
+  .getElementById("hoverCardFive")
+  .addEventListener("mouseenter", function () {
+    document.getElementById("hoverCardFive").style.backgroundColor =
+      random_bg_color("hoverCardFive");
+  });
 
-document.getElementById('hoverCardFive').addEventListener('mouseenter',function(){
-    document.getElementById('hoverCardFive').style.backgroundColor = random_bg_color('hoverCardFive');
-})
-
-document.getElementById('hoverCardSix').addEventListener('mouseenter',function(){
-    document.getElementById('hoverCardSix').style.backgroundColor = random_bg_color('hoverCardSix');
-})
+document
+  .getElementById("hoverCardSix")
+  .addEventListener("mouseenter", function () {
+    document.getElementById("hoverCardSix").style.backgroundColor =
+      random_bg_color("hoverCardSix");
+  });
--- js/item__calculate.js
+++ js/item__calculate.js
@@ -1,169 +1,195 @@
-function inputToNumber(inputNmuberIdOne, inputNmuberIdTwo){
+function inputToNumber(inputNmuberIdOne, inputNmuberIdTwo) {
+  const inputElementOne = document.getElementById(inputNmuberIdOne).value;
+  inputElementOne.value = "";
+  const inputNumberOne = parseInt(inputElementOne);
 
-    const inputElementOne = document.getElementById(inputNmuberIdOne).value;
-    inputElementOne.value = '';
-    const inputNumberOne = parseInt(inputElementOne);
-
-    const inputElementTwo = document.getElementById(inputNmuberIdTwo).value;
-    inputElementTwo.value = '';
-    const inputNumberTwo = parseInt(inputElementTwo);
-    const twoInputMultipication = inputNumberOne * inputNumberTwo;
-    return twoInputMultipication;
+  const inputElementTwo = document.getElementById(inputNmuberIdTwo).value;
+  inputElementTwo.value = "";
+  const inputNumberTwo = parseInt(inputElementTwo);
+  const twoInputMultipication = inputNumberOne * inputNumberTwo;
+  return twoInputMultipication;
 }
 
-function getCardItemName(cardNameId){
-    const itemName = document.getElementById(cardNameId).innerText;
-    return itemName;
+function getCardItemName(cardNameId) {
+  const itemName = document.getElementById(cardNameId).innerText;
+  return itemName;
 }
 
-function appendElementTable(Pname,PResult){
-    const tableContainer = document.getElementById('table-body-container')
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
+function appendElementTable(Pname, PResult) {
+  let pNameFloat = parseFloat(Pname);
+
+  const tableContainer = document.getElementById("table-body-container");
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
     <td>${serial}</td>
     <td>${PResult}</td>
-    <td>${Pname}</td>
-    <td><button style="background-color: orange; padding:20px border; border-radius: 5px;color:white;">Convert to m</button><sup>2</sup></td>
-    `;
-    tableContainer.appendChild(tr);
+    <td>${pNameFloat.toFixed(2)} cm</td>
+    <td>
+      <div class="bg-orange-400 rounded text-white">
+        <button id="convertButton" style="padding: 10px">Convert to m<sup>2</sup></button>
+      </div>
+    </td>
+  `;
+
+  tableContainer.appendChild(tr);
+
+  const button = tr.querySelector("#convertButton");
+  const resultCell = tr.querySelector("td:nth-child(3)"); // Select the third column (where the value is displayed)
+
+  button.addEventListener("click", function () {
+    const result = pNameFloat * 0.1;
+    console.log(result);
+
+    // Update the content of the result cell with the calculated value
+    resultCell.textContent = `${result} m`;
+  });
 }
 
-function nullValidationChek(inputOneChek,inputTwoChek){
-    const elementOneVlid = document.getElementById(inputOneChek).value.length;
-    const elementTwoVlid = document.getElementById(inputTwoChek).value.length;
+function nullValidationChek(inputOneChek, inputTwoChek) {
+  const elementOneVlid = document.getElementById(inputOneChek).value.length;
+  const elementTwoVlid = document.getElementById(inputTwoChek).value.length;
 
-    return (elementOneVlid+elementTwoVlid);
+  return elementOneVlid + elementTwoVlid;
 }
 
 let serial = 0;
 
+document.getElementById("btn-triangle").addEventListener("click", function () {
+  const validaton = nullValidationChek("traingle-input-b", "traingle-input-h");
+  serial++;
+  const triangleInput = inputToNumber("traingle-input-b", "traingle-input-h");
 
-document.getElementById('btn-triangle').addEventListener('click', function(){
-    const validaton = nullValidationChek('traingle-input-b','traingle-input-h')
-    serial ++;
-    const triangleInput = inputToNumber('traingle-input-b','traingle-input-h')
+  if (triangleInput <= 0) {
+    alert("Please Provide Number");
+    return;
+  } else {
+    const traingleInputResult = (triangleInput * 0.5).toFixed(2);
+    const triangleName = getCardItemName("triangle-titel");
 
-    if(triangleInput <= 0){
-        alert('Please Provide Number')
-        return
-    }else{
-        const traingleInputResult = (triangleInput*0.5).toFixed(2);
-        const triangleName = getCardItemName('triangle-titel');
-
-        if(validaton <= 0){
-            alert('Please Input Number')
-            return
-        }else{
-            appendElementTable(traingleInputResult,triangleName);
-        }
-        
+    if (validaton <= 0) {
+      alert("Please Input Number");
+      return;
+    } else {
+      appendElementTable(traingleInputResult, triangleName);
     }
+  }
+});
 
-})
+document.getElementById("btn-ractangle").addEventListener("click", function () {
+  const validaton = nullValidationChek(
+    "ractangle-input-w",
+    "ractangle-input-l"
+  );
+  serial++;
+  const RactangleInput = inputToNumber(
+    "ractangle-input-w",
+    "ractangle-input-l"
+  );
 
-document.getElementById('btn-ractangle').addEventListener('click', function(){
-    const validaton = nullValidationChek('ractangle-input-w','ractangle-input-l')
-    serial ++;
-    const RactangleInput = inputToNumber('ractangle-input-w','ractangle-input-l')
+  if (RactangleInput <= 0) {
+    alert("Please Provide Number");
+    return;
+  } else {
+    const ractangleInputResult = RactangleInput.toFixed(2);
+    const RactanlgeName = getCardItemName("ractangle-titel");
 
-    if(RactangleInput <= 0){
-        alert('Please Provide Number')
-        return
-    }else{
-        const ractangleInputResult = (RactangleInput).toFixed(2);
-        const RactanlgeName = getCardItemName('ractangle-titel');
-
-        if(validaton <= 0){
-            alert('Please Input Number')
-            return
-        }else{
-            appendElementTable(ractangleInputResult,RactanlgeName);
-        }
+    if (validaton <= 0) {
+      alert("Please Input Number");
+      return;
+    } else {
+      appendElementTable(ractangleInputResult, RactanlgeName);
     }
-})
+  }
+});
 
-document.getElementById('btn-parallelogram').addEventListener('click', function(){
-    const validaton = nullValidationChek('parallelogram-input-b','parallelogram-input-h')
-    serial ++;
-    const parallelogramInput = inputToNumber('parallelogram-input-b','parallelogram-input-h')
+document
+  .getElementById("btn-parallelogram")
+  .addEventListener("click", function () {
+    const validaton = nullValidationChek(
+      "parallelogram-input-b",
+      "parallelogram-input-h"
+    );
+    serial++;
+    const parallelogramInput = inputToNumber(
+      "parallelogram-input-b",
+      "parallelogram-input-h"
+    );
 
-    if(parallelogramInput <= 0){
-        alert('Please Provide Number')
-        return
-    }else{
-        const parallelogramInputResult = (parallelogramInput).toFixed(2);
-        const parallelogramName = getCardItemName('parallelogram-titel');
+    if (parallelogramInput <= 0) {
+      alert("Please Provide Number");
+      return;
+    } else {
+      const parallelogramInputResult = parallelogramInput.toFixed(2);
+      const parallelogramName = getCardItemName("parallelogram-titel");
 
-        if(validaton <= 0){
-            alert('Please Input Number')
-            return
-        }else{
-            appendElementTable(parallelogramInputResult,parallelogramName);
-        }
+      if (validaton <= 0) {
+        alert("Please Input Number");
+        return;
+      } else {
+        appendElementTable(parallelogramInputResult, parallelogramName);
+      }
     }
-})
+  });
 
-document.getElementById('btn-rombus').addEventListener('click', function(){
-    const validaton = nullValidationChek('rombus-input-d1','rombus-input-d2')
-    serial ++;
-    const RombusInput = inputToNumber('rombus-input-d1','rombus-input-d2')
+document.getElementById("btn-rombus").addEventListener("click", function () {
+  const validaton = nullValidationChek("rombus-input-d1", "rombus-input-d2");
+  serial++;
+  const RombusInput = inputToNumber("rombus-input-d1", "rombus-input-d2");
 
-    if(RombusInput <= 0){
-        alert('Please Provide Number')
-        return
-    }else{
-        const rombusInputResult = (RombusInput*0.5).toFixed(2);
-        const rombusName = getCardItemName('rombus-titel');
+  if (RombusInput <= 0) {
+    alert("Please Provide Number");
+    return;
+  } else {
+    const rombusInputResult = (RombusInput * 0.5).toFixed(2);
+    const rombusName = getCardItemName("rombus-titel");
 
-        if(validaton <= 0){
-            alert('Please Input Number')
-            return
-        }else{
-            appendElementTable(rombusInputResult,rombusName);
-        }
+    if (validaton <= 0) {
+      alert("Please Input Number");
+      return;
+    } else {
+      appendElementTable(rombusInputResult, rombusName);
     }
-})
+  }
+});
 
-document.getElementById('btn-pentagon').addEventListener('click', function(){
-    const validaton = nullValidationChek('pentagon-input-p','pentagon-input-b')
-    serial ++;
-    const PentagonInput = inputToNumber('pentagon-input-p','pentagon-input-b')
+document.getElementById("btn-pentagon").addEventListener("click", function () {
+  const validaton = nullValidationChek("pentagon-input-p", "pentagon-input-b");
+  serial++;
+  const PentagonInput = inputToNumber("pentagon-input-p", "pentagon-input-b");
 
-    if(PentagonInput <= 0){
-        alert('Please Provide Number')
-        return
-    }else{
-        const pentagonInputResult = (PentagonInput*0.5).toFixed(2);
-        const pentagonName = getCardItemName('pentagon-titel');
+  if (PentagonInput <= 0) {
+    alert("Please Provide Number");
+    return;
+  } else {
+    const pentagonInputResult = (PentagonInput * 0.5).toFixed(2);
+    const pentagonName = getCardItemName("pentagon-titel");
 
-        if(validaton <= 0){
-            alert('Please Input Number')
-            return
-        }else{
-            appendElementTable(pentagonInputResult,pentagonName);
-        }
+    if (validaton <= 0) {
+      alert("Please Input Number");
+      return;
+    } else {
+      appendElementTable(pentagonInputResult, pentagonName);
     }
-})
+  }
+});
 
+document.getElementById("btn-ellips").addEventListener("click", function () {
+  const validaton = nullValidationChek("ellips-input-a", "ellips-input-b");
+  serial++;
+  const ellipsInput = inputToNumber("ellips-input-a", "ellips-input-b");
 
-document.getElementById('btn-ellips').addEventListener('click', function(){
-    const validaton = nullValidationChek('ellips-input-a','ellips-input-b')
-    serial ++;
-    const ellipsInput = inputToNumber('ellips-input-a','ellips-input-b')
+  if (ellipsInput <= 0) {
+    alert("Please Provide Number");
+    return;
+  } else {
+    const ellipsInputResult = (ellipsInput * 3.1416).toFixed(2);
+    const ellipsName = getCardItemName("ellips-titel");
 
-    if(ellipsInput <= 0){
-        alert('Please Provide Number')
-        return
-    }else{
-        const ellipsInputResult = (ellipsInput*3.1416).toFixed(2);
-        const ellipsName = getCardItemName('ellips-titel');
-
-        if(validaton <= 0){
-            alert('Please Input Number')
-            return
-        }else{
-            appendElementTable(ellipsInputResult,ellipsName);
-        }
+    if (validaton <= 0) {
+      alert("Please Input Number");
+      return;
+    } else {
+      appendElementTable(ellipsInputResult, ellipsName);
     }
-})
-
+  }
+});
